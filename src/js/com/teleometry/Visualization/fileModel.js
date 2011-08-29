04c62fdd0c44095ec2fe80f63f124909e1a65aa4
atoltoetox <atoltoetox@google.com>
2011-08-29 19:55:02 +0000

    Sourcilloscope filtering UX

--- src/js/com/teleometry/Visualization/fileModel.js
+++ src/js/com/teleometry/Visualization/fileModel.js
@@ -1,120 +1,94 @@
-
 // checking to see whether file or line should be added
-var checkFile = ( function()
-{
-    function FileObject()
-    {
+var checkFile = (function () {
+    "use strict";
 
+    function FileObject(name) {
         //line object.
         //Holds line number and message send information
         //Keeps an array of nodes corresponding to a message send
-        function LineObject()
-        {
+        function LineObject(span, message, isgot) {
 
-            this.span;
-            this.isgot; //if is a got
-            this.message;
+            this.span = span;
+            this.isgot = isgot; //if is a got
+            this.message = message;
             this.show = 1;
 
-            this.ycoord;
+            this.ycoord = 0;
 
             this.lnElements = [];
 
             //adding a node to the line object
-            this.addElementToLine = function ( element )
-            {
-                this.lnElements.push( element );
-            }
+            LineObject.prototype.addElementToLine = function (element) {
+                this.lnElements.push(element);
+            };
 
-        };
+        }
 
         //file name
-        this.name;
+        this.name = name;
       
         //array of line objects
         this.lines = [];
 
-        //sets file information
-        this.setFile = function( name, span, message, element, ifgot )
-        {
-            this.name = name;
-            this.addLine( span, message, element, ifgot );
-        };
-
         //adds line to file
-        this.addLine = function( span, message, element, ifgot )
-        {
+        FileObject.prototype.addLine = function (span, message, element, ifgot) { 
 
             //creates an instance of the line object
-            var lobj = new LineObject();
-            lobj.span = span;
-            lobj.isgot = ifgot;
-            lobj.message = message;
-            lobj.addElementToLine( element );
+            var i, lobj = new LineObject(span, message, ifgot);
+            lobj.addElementToLine(element);
 
-            if ( this.lines.length == 0 )
-            {
-                this.lines.push( lobj );
-                return;
-            }
-            else
-            { 
-                var i;
-                for ( i = 0; i < this.lines.length; i++ )
-                {
-                    if ( span[0][0] < this.lines[i].span[0][0] )
-                    {
-                        this.lines.splice( i, 0, lobj );
+            for (i = 0; i < this.lines.length; i += 1) {
+
+                if (span[0][0] < this.lines[i].span[0][0]) {
+                    this.lines.splice(i, 0, lobj);
+                    return;
+                }
+                if (span[0][0] === this.lines[i].span[0][0]) {
+                    //put got after sent from same source line
+                    if (!ifgot) { 
+                        this.lines.splice(i, 0, lobj);
                         return;
                     }
                 }
-                this.lines.push( lobj );
             }
-
-            //this.lines.push( lobj );
+            this.lines.push(lobj);
         };
-
-    };
-
-    function addFile( files, name, span, message, element, ifgot )
-    {
-        fobj = new FileObject();
-        fobj.setFile( name, span, message, element, ifgot );
-        files.push( fobj );
     }
 
-    return function( files, name, span, message, element, ifgot )
-    {
+    function addFile(files, name, span, message, element, ifgot) {
+        var fobj = new FileObject(name);
+        fobj.addLine(span, message, element, ifgot);
+        files.push(fobj);
+    }
 
-        var i;
-        for ( i = 0; i < files.length; i++ )
-        {
-            if ( files[i].name == name )
-            {
+    return function (files, name, span, message, element, ifgot) {
+
+        var i, j, line;
+        for (i = 0; i < files.length; i += 1) {
+
+            if (files[i].name === name) {
                 //check for line
-                var j;
-                for ( j = 0; j < files[i].lines.length; j++ )
-                {
-                    if ( files[i].lines[j].span[0][0] == span[0][0] && files[i].lines[j].span[0][1] == span[0][1])
-                    {
-                        files[i].lines[j].addElementToLine( element );
+                for (j = 0; j < files[i].lines.length; j += 1) {
+                    line = files[i].lines[j];
+                    if (line.span[0][0] === span[0][0] && line.span[0][1] === span[0][1]) {
+                        line.addElementToLine(element);
                         return;
                     }
                 }
             
                 //line not found, add line to file
-                files[i].addLine( span, message, element, ifgot );
+                files[i].addLine(span, message, element, ifgot);
                 return;
             }
    
         }//for i
 
         //file was not found, create new file object
-        addFile( files, name, span, message, element, ifgot );
+        addFile(files, name, span, message, element, ifgot);
 
     };
 
 
-}() );
+}());
 
 
